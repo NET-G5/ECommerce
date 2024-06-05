7a82aeb8aaf97efe134c6de302c88f2c366418eb
Jamshidbek <161879703+jamshidbekbahtiyorivich@users.noreply.github.com>
2024-06-05 17:40:32 +0500

    updated

---
+++ Desktop/ECommerce/ECommerce/Views/Views/AuthorizationView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace ECommerce.Views.Views;
+
+/// <summary>
+/// Interaction logic for AuthorizationView.xaml
+/// </summary>
+public partial class AuthorizationView : UserControl
+{
+    public AuthorizationView()
+    {
+        InitializeComponent();
+    }
+}

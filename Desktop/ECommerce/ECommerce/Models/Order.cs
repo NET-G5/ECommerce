b820e70a92874892666c4c2687246b23a9564907
Ramadan1011 <shukhratovich75@gmail.com>
2024-06-05 10:04:56 +0500

    completed migrations

---
+++ Desktop/ECommerce/ECommerce/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace ECommerce.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    public UserAccount UserAccount { get; set; }
+    public int UserAccountId { get; set; }
+
+    public Product Product { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+    public OrderStatus OrderStatus { get; set; }
+    public DateTime CreatedDate { get; set; }
+
+}
---
+++ Desktop/ECommerce/ECommerce/Models/Reviews.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Models;
+
+public class Reviews
+{
+    public int Id { get; set; }
+    public string Review { get; set; }
+
+    public UserAccount UserAccount { get; set; }
+    public int UserAccountId { get; set; }
+
+    public Product Product { get; set; }
+    public int ProductId { get; set; }
+}

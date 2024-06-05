aa92ce635855e0f657ed92c11aafe108ebd545ad
Jamshidbek <161879703+jamshidbekbahtiyorivich@users.noreply.github.com>
2024-06-05 23:55:23 +0500

    changes

---
+++ Desktop/ECommerce/ECommerce/Views/Views/BasketView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace ECommerce.Views.Views;
+
+/// <summary>
+/// Interaction logic for BasketView.xaml
+/// </summary>
+public partial class BasketView : UserControl
+{
+    public ObservableCollection<Product> Products { get; set; }
+    public BasketView()
+    {
+        InitializeComponent();
+
+
+        Products = new ObservableCollection<Product>
+        {
+            new Product { Name = "Carolina Herrera Good Girl", Description = "Eau de parfum", Price = 156, ImageUrl = @"C:\Users\Пользователь\OneDrive\Rasmlar\Ekran rasmlari\Screenshot 2024-06-05 232204.png" },
+            new Product { Name = "Creed Aventus", Description = "Eau de parfum", Price = 152, ImageUrl = @"C:\Users\Пользователь\OneDrive\Rasmlar\Ekran rasmlari\Screenshot 2024-06-05 232204.png"}
+        };
+
+        OrderDataGrid.ItemsSource = Products;
+    }
+
+}

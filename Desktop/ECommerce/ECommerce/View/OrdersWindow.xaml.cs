1cc9be84a0e62fb5725738cdcaa74632abc73125
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-07-01 15:55:55 +0500

    add images folders and View folder

---
+++ Desktop/ECommerce/ECommerce/View/OrdersWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+namespace ECommerce.View
+{
+    /// <summary>
+    /// Interaction logic for OrdersWindow.xaml
+    /// </summary>
+    public partial class OrdersWindow : Window
+    {
+        public OrdersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+        private bool isMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (isMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Height = 720;
+                    this.Width = 1080;
+                    isMaximized = false;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    isMaximized = true;
+                }
+            }
+
+        }
+    }
+}   

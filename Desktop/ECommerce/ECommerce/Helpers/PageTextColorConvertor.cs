a50cfca4a3938645207a1876837d655602eda417
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-07-02 18:33:48 +0500

    add helpers folder and added sub classes

---
+++ Desktop/ECommerce/ECommerce/Helpers/PageTextColorConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ECommerce.Helpers
+{
+    internal class PageTextColorConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return new SolidColorBrush(Colors.White);
+            }
+
+            return new SolidColorBrush(Colors.Blue);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

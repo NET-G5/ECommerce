611f20cd75acb77273c9f6d1fab33ba3b8071d5f
Ramadan1011 <shukhratovich75@gmail.com>
2024-07-03 12:45:13 +0500

    connected login and orders pages

--- Desktop/ECommerce/ECommerce/MainWindow.xaml.cs
+++ Desktop/ECommerce/ECommerce/MainWindow.xaml.cs
@@ -11,10 +11,28 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    private void Button_Click(object sender, RoutedEventArgs e)
+    private void BtnLogin_Clicked(object sender, RoutedEventArgs e)
     {
-        var window =new OrderDetailsView();
-        window.Show();
+        string username = UsernameInput.Text;
+        string password = PasswordInput.Password.ToString();
+        if (!CheckAdmin(username, password))
+        {
+            return;
+        }
+
+        var ordersView = new OrdersView();
+        ordersView.Show();
+    }
+
+    private bool CheckAdmin(string username, string password)
+    {
+        if (username == "pdp" && password == "pdp123")
+        {
+            return true;
+        }
+
+        MessageBox.Show("Username or password is incorrect. Please, try again!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        return false;
     }
 
     private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -33,5 +51,10 @@ public partial class MainWindow : Window
     private void BtnClose_Click(object sender, RoutedEventArgs e)
     {
         Application.Current.Shutdown();
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+
     }
 }

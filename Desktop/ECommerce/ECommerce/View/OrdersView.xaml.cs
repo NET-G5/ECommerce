8d3972619dcc43067e0d7add8656dafab796275d
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-07-04 23:15:50 +0500

    any changes

--- Desktop/ECommerce/ECommerce/View/OrdersView.xaml.cs
+++ Desktop/ECommerce/ECommerce/View/OrdersView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ECommerce.Models;
+using ECommerce.Services;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,43 +9,24 @@ namespace ECommerce.View;
 public partial class OrdersView : Window
 {
     private readonly Employee loggedInEmployee;
-	private bool isMaximized = false;
-
-	public OrdersView()
+    private readonly OrderService _orderService;
+     ObservableCollection<Order> _orders;
+    public OrdersView()
     {
-        InitializeComponent();
+        InitializeComponent();  
+        _orderService = new OrderService();
+        _orders = new ObservableCollection<Order>();
     }
 
     public OrdersView(Employee employee)
     {
         loggedInEmployee = employee;
     }
+    void Load()
+    {
 
-    private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-    {
-        if (e.ClickCount == 2)
-        {
-            if (isMaximized)
-            {
-                WindowState = WindowState.Normal;
-                Height = 720;
-                Width = 1080;
-                isMaximized = false;
-            }
-            else
-            {
-                WindowState = WindowState.Maximized;
-                isMaximized = true;
-            }
-        }
     }
-    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-    {
-        if (e.LeftButton == MouseButtonState.Pressed)
-        {
-            DragMove();
-        }
-    }
+
 
     private void BtnMinimize_Click(object sender, RoutedEventArgs e)
     {
@@ -53,5 +36,10 @@ public partial class OrdersView : Window
     private void BtnClose_Click(object sender, RoutedEventArgs e)
     {
         Application.Current.Shutdown();
+    }
+
+    private void LogOut_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }

257470b7ec37e042c50c06f11385e5e104b79ee3
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-07-05 05:34:51 +0500

    Updates in views

--- Desktop/ECommerce/ECommerce/View/OrdersView.xaml.cs
+++ Desktop/ECommerce/ECommerce/View/OrdersView.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using ECommerce.Models;
 using ECommerce.Services;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
 using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace ECommerce.View;
@@ -10,21 +12,43 @@ public partial class OrdersView : Window
 {
     private readonly Employee loggedInEmployee;
     private readonly OrderService _orderService;
-     ObservableCollection<Order> _orders;
-    public OrdersView()
-    {
-        InitializeComponent();  
-        _orderService = new OrderService();
-        _orders = new ObservableCollection<Order>();
-    }
-
+    public ObservableCollection<Order> Orders;
+    private int? _orderId;
+    private string? _customer="";
+    private OrderStatus? _orderStatus;
+   
     public OrdersView(Employee employee)
     {
-        loggedInEmployee = employee;
-    }
-    void Load()
-    {
+        InitializeComponent();  
 
+        string fullName=employee.LastName+" "+employee.FirstName;
+        EmpTitle.TextDecorations.Clear();
+        EmpTitle.Text = fullName;
+
+        _orderService = new OrderService();
+        Orders = new ObservableCollection<Order>();
+        
+        if (!string.IsNullOrEmpty(OrderIdSearchText.Text))
+            _orderId = int.Parse(OrderIdSearchText.Text);
+        else _orderId = null;
+        _customer = CustomerNameSearchText.Text??"";
+        if(StatusCombobox.SelectedItem != null )
+            _orderStatus=(OrderStatus)StatusCombobox.SelectedItem;
+        else _orderStatus = null;
+        
+        Load(_orderId,_customer,_orderStatus);
+        OrdersDataGrid.ItemsSource = Orders;
+
+    }
+    
+    void Load(int? OrderId ,string Customer, OrderStatus? Status)
+    {
+        var orders = _orderService.GetOrder(OrderId,Customer,Status);
+        Orders.Clear();
+        foreach (var order in orders)
+        {
+            Orders.Add(order);   
+        }
     }
 
 
@@ -40,6 +64,18 @@ public partial class OrdersView : Window
 
     private void LogOut_Click(object sender, RoutedEventArgs e)
     {
-
+        var window = new MainWindow();
+        window.Show();
+        this.Close();
+    }
+    private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        var selectedOrder=OrdersDataGrid.SelectedItem as Order;
+        if (selectedOrder is not null)
+        {
+            var window = new OrderDetailsView();
+            window.Show();
+            this.Close();
+        }
     }
 }

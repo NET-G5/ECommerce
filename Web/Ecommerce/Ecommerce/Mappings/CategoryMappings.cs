237a273d921973146be882f8fa4087034908fd73
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-21 16:22:03 +0500

    Mappings folder

---
+++ Web/Ecommerce/Ecommerce/Mappings/CategoryMappings.cs
@@ -0,0 +1,45 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.ViewModels.Category;
+
+namespace Ecommerce.Mappings
+{
+    public static class CategoryMappings
+    {
+        public static CategoryViewModel ToViewModel(this Category category)
+        {
+            return new CategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description,
+            };
+        }
+        public static UpdateCategoryViewModel ToUpdateViewModel(this CategoryViewModel category)
+        {
+            return new UpdateCategoryViewModel
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description,
+            };
+        }
+        public static Category ToEntity(this CreateCategoryViewModel category)
+        {
+            return new Category
+            {
+                Name = category.Name,
+                Description = category.Description,
+            };
+        }
+        public static Category ToEntity(this UpdateCategoryViewModel category)
+        {
+            return new Category
+            {
+                Id = category.Id,
+                Name = category.Name,
+                Description = category.Description,
+            };
+        }
+
+    }
+}
---
+++ Web/Ecommerce/Ecommerce/Mappings/PaymentDetailMappings.cs
@@ -0,0 +1,52 @@
+﻿using Ecommerce.Domain.Entities;
+using Ecommerce.ViewModels.PaymentDetail;
+
+namespace Ecommerce.Mappings
+{
+    public static class PaymentDetailMappings
+    {
+        public static PaymentDetailViewModel ToViewModel(this PaymentDetail paymentDetail)
+        {
+            return new PaymentDetailViewModel
+            {
+                Id = paymentDetail.Id,
+                Method = paymentDetail.Method,
+                PaymentDate = paymentDetail.PaymentDate,
+                Amount = paymentDetail.Amount,
+                OrderId = paymentDetail.OrderId,
+            };
+        }
+        public static UpdatePaymentDetailViewModel ToUpdateViewModel(this PaymentDetailViewModel paymentDetail)
+        {
+            return new UpdatePaymentDetailViewModel
+            {
+                Id = paymentDetail.Id,
+                Method = paymentDetail.Method,
+                PaymentDate = paymentDetail.PaymentDate,
+                Amount = paymentDetail.Amount,
+                OrderId = paymentDetail.OrderId,
+            };
+        }
+        public static PaymentDetail ToEntity(this CreatePaymentDetailViewModel paymentDetail)
+        {
+            return new PaymentDetail
+            {
+                Method = paymentDetail.Method,
+                PaymentDate = paymentDetail.PaymentDate,
+                Amount = paymentDetail.Amount,
+                OrderId = paymentDetail.OrderId,
+            };
+        }
+        public static PaymentDetail ToEntity(this UpdatePaymentDetailViewModel paymentDetail)
+        {
+            return new PaymentDetail
+            {
+                Id= paymentDetail.Id,
+                Method = paymentDetail.Method,
+                PaymentDate = paymentDetail.PaymentDate,
+                Amount = paymentDetail.Amount,
+                OrderId = paymentDetail.OrderId,
+            };
+        }
+    }
+}

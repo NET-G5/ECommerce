d0cc34ab62b82fa05267dda4444093e45bcefeb4
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-08-21 12:30:53 +0500

    all configurations

---
+++ Web/Ecommerce/Ecommerce.Infrastructure/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            builder.HasMany(x => x.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId);
+
+        }
+    }
+}
---
+++ Web/Ecommerce/Ecommerce.Infrastructure/Persistence/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Infrastructure.Persistence.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItem");
+
+            builder.HasOne(x => x.Order)
+                .WithMany(o => o.Items)
+                .HasForeignKey(o => o.OrderId);
+
+            builder.HasOne(p=>p.Product)
+                .WithOne(x=>x.OrderItem)
+                .HasForeignKey<OrderItem>(o => o.ProductId);
+        }
+    }
+}

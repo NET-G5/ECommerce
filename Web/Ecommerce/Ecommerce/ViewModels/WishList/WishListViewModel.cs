a271928e4747e767cbc435293a889be022bf7315
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-09-01 13:18:54 +0500

    wishListViewModel

--- Web/Ecommerce/Ecommerce/ViewModels/WishList/WishListViewModel.cs
+++ Web/Ecommerce/Ecommerce/ViewModels/WishList/WishListViewModel.cs
@@ -1,9 +1,12 @@
-﻿namespace Ecommerce.ViewModels.WishList
+﻿using Ecommerce.ViewModels.Product;
+
+namespace Ecommerce.ViewModels.WishList
 {
     public class WishListViewModel
     {
         public int Id { get; set; }
         public int CustomerId { get; set; }
+        public ICollection<ProductViewModel> Products { get; set; }
 
     }
 }

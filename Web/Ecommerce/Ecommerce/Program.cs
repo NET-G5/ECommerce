82161b0aec7bc99416fe639d15fec8ba6721a5aa
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-09-20 18:05:30 +0500

    program cs file changes

--- Web/Ecommerce/Ecommerce/Program.cs
+++ Web/Ecommerce/Ecommerce/Program.cs
@@ -3,7 +3,9 @@ using Ecommerce.Infrastructure.Persistence;
 using Ecommerce.Infrastructure.Repositories;
 using Ecommerce.Services;
 using Ecommerce.Services.Interfaces;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce;
@@ -24,11 +26,7 @@ public class Program
         builder.Services.AddDbContext<EcommerceDbContext>(options =>
             options.UseSqlServer(connectionString));
 
-        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(options => { options.ExpireTimeSpan = TimeSpan.FromSeconds(10); });
-
         builder.Services.AddScoped<ICommonRepository, CommonRepository>();
-
 
         builder.Services.AddScoped<IPaymentDetailRepository, PaymentDetailRepository>();
         builder.Services.AddScoped<IPaymentDetailService, PaymentDetailService>();
@@ -58,6 +56,49 @@ public class Program
         builder.Services.AddScoped<IWishListService, WishListService>();
 
 
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+            options.LoginPath = "/Account/Login";
+            options.AccessDeniedPath = "/Account/AccessDenied";
+            options.SlidingExpiration = true;
+        });
+
+        builder.Services
+           .AddIdentity<IdentityUser, IdentityRole>(options =>
+           {
+               options.SignIn.RequireConfirmedAccount = false;
+               options.SignIn.RequireConfirmedPhoneNumber = false;
+
+               options.Password.RequireDigit = true;
+               options.Password.RequireLowercase = true;
+               options.Password.RequireUppercase = true;
+               options.Password.RequireNonAlphanumeric = true;
+               options.Password.RequiredLength = 8;
+
+               options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+               options.Lockout.MaxFailedAccessAttempts = 5;
+               options.Lockout.AllowedForNewUsers = true;
+
+               options.User.RequireUniqueEmail = true;
+           })
+           .AddEntityFrameworkStores<EcommerceDbContext>()
+           .AddDefaultTokenProviders();
+
+        builder.Services
+            .AddControllers(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            })
+            .AddJsonOptions(x =>
+            {
+                x.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
         var app = builder.Build();
 
         if (!app.Environment.IsDevelopment())
@@ -72,7 +113,7 @@ public class Program
         app.UseRouting();
 
         app.UseAuthentication();
-        
+
         app.UseAuthorization();
 
         app.MapControllerRoute(

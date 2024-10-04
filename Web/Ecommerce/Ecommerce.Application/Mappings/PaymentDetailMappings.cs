8cba978764cbc35cac809963cd88e2c469ddf1d9
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2024-10-04 11:04:31 -0700

    Update PaymentDetailMappings.cs

--- Web/Ecommerce/Ecommerce.Application/Mappings/PaymentDetailMappings.cs
+++ Web/Ecommerce/Ecommerce.Application/Mappings/PaymentDetailMappings.cs
@@ -1,51 +1,53 @@
 ﻿using Ecommerce.Application.ViewModels.PaymentDetail;
 using Ecommerce.Domain.Entities;
 
-namespace Ecommerce.Application.Mappings
+namespace Ecommerce.Application.Mappings;
+
+public static class PaymentDetailMappings
 {
-    public static class PaymentDetailMappings
+    public static PaymentDetailViewModel ToViewModel(this PaymentDetail paymentDetail)
     {
-        public static PaymentDetailViewModel ToViewModel(this PaymentDetail paymentDetail)
+        return new PaymentDetailViewModel
         {
-            return new PaymentDetailViewModel
-            {
-                Id = paymentDetail.Id,
-                Method = paymentDetail.Method,
-                PaymentDate = paymentDetail.PaymentDate,
-                Amount = paymentDetail.Amount,
-                OrderId = paymentDetail.OrderId,
-            };
-        }
-        public static UpdatePaymentDetailViewModel ToUpdateViewModel(this PaymentDetailViewModel paymentDetail)
+            Id = paymentDetail.Id,
+            Method = paymentDetail.Method,
+            PaymentDate = paymentDetail.PaymentDate,
+            Amount = paymentDetail.Amount,
+            OrderId = paymentDetail.OrderId,
+        };
+    }
+    
+    public static UpdatePaymentDetailViewModel ToUpdateViewModel(this PaymentDetailViewModel paymentDetail)
+    {
+        return new UpdatePaymentDetailViewModel
         {
-            return new UpdatePaymentDetailViewModel
-            {
-                Id = paymentDetail.Id,
-                Method = paymentDetail.Method,
-                PaymentDate = paymentDetail.PaymentDate,
-                Amount = paymentDetail.Amount,
-                OrderId = paymentDetail.OrderId,
-            };
-        }
-        public static PaymentDetail ToEntity(this CreatePaymentDetailViewModel paymentDetail)
+            Id = paymentDetail.Id,
+            Method = paymentDetail.Method,
+            PaymentDate = paymentDetail.PaymentDate,
+            Amount = paymentDetail.Amount,
+            OrderId = paymentDetail.OrderId,
+        };
+    }
+    
+    public static PaymentDetail ToEntity(this CreatePaymentDetailViewModel paymentDetail)
+    {
+        return new PaymentDetail
         {
-            return new PaymentDetail
-            {
-                Method = paymentDetail.Method,
-                PaymentDate = paymentDetail.PaymentDate,
-                Amount = paymentDetail.Amount,
-                OrderId = paymentDetail.OrderId,
-            };
-        }
-        public static PaymentDetail ToEntity(this PaymentDetailViewModel paymentDetail)
+            Method = paymentDetail.Method,
+            PaymentDate = paymentDetail.PaymentDate,
+            Amount = paymentDetail.Amount,
+            OrderId = paymentDetail.OrderId,
+        };
+    }
+    
+    public static PaymentDetail ToEntity(this PaymentDetailViewModel paymentDetail)
+    {
+        return new PaymentDetail
         {
-            return new PaymentDetail
-            {
-                Method = paymentDetail.Method,
-                PaymentDate = paymentDetail.PaymentDate,
-                Amount = paymentDetail.Amount,
-                OrderId = paymentDetail.OrderId,
-            };
-        }
+            Method = paymentDetail.Method,
+            PaymentDate = paymentDetail.PaymentDate,
+            Amount = paymentDetail.Amount,
+            OrderId = paymentDetail.OrderId,
+        };
     }
 }
